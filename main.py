2f6e9e1f15700a76b6a5ef53436b82d36cab7ae9
alsarna <aleksandra.sarna97@gmail.com>
2022-06-12 12:44:13 +0200

    output file + topic in pykafka

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from pyflink.common import JsonRowDeserializationSchema, Types, WatermarkStrateg
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import FlinkKafkaConsumer
 from pyflink.table import StreamTableEnvironment
+from pyflink.datastream.connectors import FileSink, OutputFileConfig
+from pyflink.common.serialization import Encoder
+
 
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
-    t_env = StreamTableEnvironment.create(stream_execution_environment = env)
+    t_env = StreamTableEnvironment.create(stream_execution_environment=env)
 
     env.add_jars("file:/home/students/s424343/PycharmProjects/kafka_proj/flink-sql-connector-kafka-1.15.0.jar")
 
     type_info = Types.ROW_NAMED(["Bitcoin", "eth", "ltc", "usd", "aud", "cad", "chf", "eur", "gbp", "pln"],
-                                [Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE()])
+                                [Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(),
+                                 Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE()])
     deserialization_schema = JsonRowDeserializationSchema.builder().type_info(type_info).build()
 
     kafkaSource = FlinkKafkaConsumer(
         topics='bitcoin',
-        deserialization_schema = deserialization_schema,
-        properties={'bootstrap.servers': '150.254.78.69:29092 ',
+        deserialization_schema=deserialization_schema,
+        properties={'bootstrap.servers': '150.254.78.69:29092',
                     'group.id': 's424343'}
     )
     kafkaSource.set_start_from_earliest()
@@ -30,10 +34,16 @@ def main():
 
     print('\ntable data')
     print(table.print_schema())
-    env.execute()
 
-    ds = t_env.to_append_stream(table, Types.ROW([[Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE(),Types.DOUBLE()]]))
-    # env.execute()
+    output_path = './output/'
+    file_sink = FileSink \
+        .for_row_format(output_path, Encoder.simple_string_encoder()) \
+        .with_output_file_config(OutputFileConfig.builder().with_part_prefix('pre').with_part_suffix('suf').build()) \
+        .build()
+
+    ds.sink_to(file_sink)
+
+    env.execute()
 
 if __name__ == '__main__':
     main()
---
+++ new_topic.py
@@ -0,0 +1,9 @@
+from pykafka import KafkaClient
+import threading
+
+KAFKA_HOST = "150.254.78.69:29092"
+
+client = KafkaClient(hosts = KAFKA_HOST)
+topic = client.topics["s424343"]
+
+cprint(lient.topics.values())
